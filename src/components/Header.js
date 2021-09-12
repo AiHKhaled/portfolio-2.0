9f1f04ec4d8a469792f3cc9e80ad8b05ab5c0db2
parzivalElijah <aithammouk40@gmail.com>
2021-09-12 15:01:47 +0100

    added languages i worked with

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,9 +15,12 @@ const Header = () => {
             This is My <span>Portfolio</span>
           </h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            commodi quia reprehenderit eum quidem odio excepturi et deleniti
-            saepe explicabo!
+            My name is Ait Hammou khaled, I'm sort of new in the web
+            developement world, I did a little bit of programming here and there
+            but not nothing too serious, some of the languages and frameworks I
+            worked with can be found if you scroll a little further down...
+            <br />
+            see you there!
           </p>
         </Description>
 
--- src/components/Middle.js
+++ src/components/Middle.js
@@ -4,9 +4,22 @@ import home2 from "../img/home2.png";
 import { motion } from "framer-motion";
 import { useScroll } from "./useScroll";
 import { fade } from "../Animation";
-
+import python from "../img/python.svg";
+import js from "../img/js.svg";
+import science from "../img/science.svg";
+import css from "../img/css.svg";
+import html from "../img/html.svg";
 const Middle = () => {
   const [element, controls] = useScroll();
+  const Languages = [
+    { name: "Js", logo: js },
+    { name: "Django", logo: python },
+    { name: "Python", logo: python },
+    { name: "React", logo: science },
+    { name: "Html", logo: html },
+    { name: "Css", logo: css },
+  ];
+
   return (
     <First variants={fade} animate={controls} initial="hidden" ref={element}>
       <Image>
@@ -23,22 +36,41 @@ const Middle = () => {
       </Image>
       <Description>
         <motion.h2
-        //animate={{ x: 100 }}
-        //transition={{ ease: "easeOut", duration: 2 }}
+          animate={{ x: 0 }}
+          initial={{ x: 100 }}
+          transition={{ ease: "easeOut", duration: 2 }}
         >
-          Something else{" "}
+          The
           <motion.span
             animate={{ opacity: 1, transition: { duration: 2 } }}
             initial={{ opacity: 0 }}
           >
-            Entirely
+            Programming
           </motion.span>
+          <br />
+          languages I worked with
         </motion.h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-          voluptas incidunt voluptate pariatur cupiditate quo sit excepturi
-          dignissimos minima harum.
-        </p>
+        <Cards>
+          {Languages.map((l) => (
+            <Card
+              whileHover={{ scale: 1.2 }}
+              whileTap={{
+                scale: 0.8,
+                borderRadius: "100%",
+              }}
+              key={l.nam}
+            >
+              <div className="icon">
+                <h3>{l.name}</h3>
+                <img
+                  style={{ width: "40px", height: "40px" }}
+                  src={l.logo}
+                  alt="logo"
+                />
+              </div>
+            </Card>
+          ))}
+        </Cards>
       </Description>
     </First>
   );
@@ -57,7 +89,7 @@ const Image = styled.div`
   float: left;
   img {
     width: 80%;
-    height: 70vh;
+    height: 65vh;
     border-radius: 10px;
     object-fit: cover;
   }
@@ -69,5 +101,31 @@ const Description = styled.div`
   padding-right: 4rem;
   padding-bottom: 12rem;
 `;
+const Card = styled(motion.div)`
+  flex-basis: 10rem;
 
+  border-radius: 5px;
+  //box-shadow: #23d997 0px 0px 0px 2px;
+  margin: 2rem 4.5rem 2rem 0rem;
+  h3 {
+    font-size: medium;
+    margin-left: 1rem;
+    padding: 1rem;
+  }
+
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    align-items: center;
+    img {
+      margin: 1rem 0rem 1rem 1rem;
+    }
+  }
+`;
+const Cards = styled.div`
+  display: flex;
+  padding-top: 2rem;
+  flex-wrap: wrap;
+`;
 export default Middle;
